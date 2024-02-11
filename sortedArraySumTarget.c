71c577d29447a4215b95ef51bbfc9cfcf63c9c78
Dinkar Thakur <322dinkar@gmail.com>
2024-02-11 18:59:34 +0530

    hi

---
+++ sortedArraySumTarget.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+  int arr[8]={1,2,3,4,5,8,9,10};
+  int n;
+  scanf("%d",&n);
+  int i=0;
+  int j=n-1;
+  while(i<j){
+    if(arr[i]+arr[j]==n){
+      printf("possible");
+      break;
+    }
+    else if(arr[i]+arr[j]>n) j--;
+    else if(arr[i]+arr[j]<n) j++;
+    else{
+      printf("not found");
+    }
+  }
+  return 0;
+  
+}
